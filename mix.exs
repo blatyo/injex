85727957d5f5613614c50c7ca1bd36911216b382
Allen Madsen <blatyo@gmail.com>
2016-09-05 20:06:32 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Injex.Mixfile do
 
   def project do
     [app: :injex,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
