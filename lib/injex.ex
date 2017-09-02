cf30ea3b9b1534be5dfe7154db314a53a0d992b1
Allen Madsen <blatyo@gmail.com>
2017-09-02 11:13:46 -0400

    Fix injection

--- lib/injex.ex
+++ lib/injex.ex
@@ -1,23 +1,13 @@
 defmodule Injex do
-  if Mix.env == :test do
-
-    defmacro inject(name, dep) do
+  defmacro inject(name, dep) do
+    if Mix.env == :test do
       quote do
-        def unquote(name)() do
-          Application.get_env(__MODULE__, unquote(name), unquote(dep))
-        end
+        def unquote(name)(), do: Application.get_env(__MODULE__, unquote(name), unquote(dep))
+      end
+    else
+      quote do
+        def unquote(name)(), do: unquote(dep)
       end
     end
-
-  else
-
-    defmacro inject(name, dep) do
-      quote do
-        def unquote(name)() do
-          unquote(dep)
-        end
-      end
-    end
-
   end
 end
